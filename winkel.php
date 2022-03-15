fa12e037a596c2055253c33b858959ac3e653654
Jack Tiebie <jacktiebie45@gmail.com>
2022-03-15 11:08:19 +0100

    :)

--- winkel.php
+++ winkel.php
@@ -1,16 +1,18 @@
-<?php
-require 'includes/db_connection.php';
+<?php 
+session_start();
+include 'includes/db_connection.php';
 
+ 
 if(isset($_POST["add_to_cart"]))
 {
  if(isset($_SESSION["shopping_cart"]))
  {
  $item_array_id = array_column($_SESSION["shopping_cart"], "item_id");
- if(!in_array($_GET["ID"], $item_array_id))
+ if(!in_array($_GET["id"], $item_array_id))
  {
  $count = count($_SESSION["shopping_cart"]);
  $item_array = array(
- 'item_id' => $_GET["ID"],
+ 'item_id' => $_GET["id"],
  'item_name' => $_POST["hidden_name"],
  'item_price' => $_POST["hidden_price"],
  'item_quantity' => $_POST["quantity"]
@@ -25,7 +27,7 @@ if(isset($_POST["add_to_cart"]))
  else
  {
  $item_array = array(
- 'item_id' => $_GET["ID"],
+ 'item_id' => $_GET["id"],
  'item_name' => $_POST["hidden_name"],
  'item_price' => $_POST["hidden_price"],
  'item_quantity' => $_POST["quantity"]
@@ -40,60 +42,49 @@ if(isset($_GET["action"]))
  {
  foreach($_SESSION["shopping_cart"] as $keys => $values)
  {
- if($values["item_id"] == $_GET["ID"])
+ if($values["item_id"] == $_GET["id"])
  {
  unset($_SESSION["shopping_cart"][$keys]);
  echo '<script>alert("Item Removed")</script>';
- echo '<script>window.location="index.php"</script>';
+ echo '<script>window.location="winkel.php"</script>';
  }
  }
  }
 }
-
-
+ 
 ?>
-
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Webshop voor het kopen van diverse medicijnen">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Medicijnen Webshop</title>
-</head>
-<body>
-    <section class="shop">
-   <?php include_once 'header.php'; ?>
-   <div class="shop__container">
-        <h2>Vind de juiste medicijnen voor u</h2>
-        <form class="hero__container__form" action="#" method="GET">
-                <input class="hero__container__form-input" placeholder="Vind medicijnen in onze webshop" type="text" name="query" />
-                <input class="hero__container__form-search" type="submit" value="Search" />
-            </form>
-    </div>
-    </section>
-    <div class="shop__grid">
-    <div class="shop__left">
-    <img src="images/placeholder_1.png" alt="">
-    </div>
-    <div class="shop__right">
-<?php
- $query = "SELECT ID, name, price FROM medicines ORDER BY ID ASC";
+<html>
+ <head>
+ <title>Jack</title>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+ </head>
+ <body>
+ <br />
+ <div class="container">
+ <br />
+ <br />
+ <br />
+ <h3>Jack <a href="https://webdevtrick.com">:)</a></h3><br />
+ <br /><br />
+ <?php
+ $query = "SELECT * FROM tbl_product ORDER BY id ASC";
  $result = mysqli_query($conn, $query);
  if(mysqli_num_rows($result) > 0)
  {
  while($row = mysqli_fetch_array($result))
  {
  ?>
-<div>
- <form method="post" action="medicijnen-winkel.php?action=add&id=<?php echo $row["ID"]; ?>">
- <img src="images/paracetamol.png" /><br />
+ <div class="col-md-4">
+ <form method="post" action="winkel.php?action=add&id=<?php echo $row["id"]; ?>">
+ <div style="border:3px solid #5cb85c; background-color:whitesmoke; border-radius:5px; padding:16px;" align="center">
+ <img src="images/<?php echo $row["image"]; ?>" class="img-responsive" /><br />
  
- <h4 class="shop__right__item__name"><?php echo $row["name"]; ?></h4>
+ <h4 class="text-info"><?php echo $row["name"]; ?></h4>
  
- <h4 class="shop__right__item__price">$ <?php echo $row["price"]; ?></h4>
+ <h4 class="text-danger">$ <?php echo $row["price"]; ?></h4>
  
  <input type="text" name="quantity" value="1" class="form-control" />
  
@@ -101,16 +92,16 @@ if(isset($_GET["action"]))
  
  <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />
  
- <input type="submit" name="add_to_cart" style="margin-top:5px;" value="Add to Cart" />
+ <input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success" value="Add to Cart" />
  
+ </div>
  </form>
  </div>
  <?php
  }
  }
  ?>
-
-<div style="clear:both"></div>
+ <div style="clear:both"></div>
  <br />
  <h3>Order Details</h3>
  <div class="table-responsive">
@@ -134,15 +125,15 @@ if(isset($_GET["action"]))
  <td><?php echo $values["item_quantity"]; ?></td>
  <td>$ <?php echo $values["item_price"]; ?></td>
  <td>$ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2);?></td>
- <td><a href="index.php?action=delete&ID=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td>
+ <td><a href="winkel.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td>
  </tr>
  <?php
  $total = $total + ($values["item_quantity"] * $values["item_price"]);
  }
  ?>
  <tr>
- <td colspan="3" >Total</td>
- <td >$ <?php echo number_format($total, 2); ?></td>
+ <td colspan="3" align="right">Total</td>
+ <td align="right">$ <?php echo number_format($total, 2); ?></td>
  <td></td>
  </tr>
  <?php
@@ -154,8 +145,5 @@ if(isset($_GET["action"]))
  </div>
  </div>
  <br />
-   
-    </div>
-    </div>
-</body>
+ </body>
 </html>
